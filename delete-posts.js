ea8d5dbefff30ed504abc7d607d83f552912bfa5
Renaud R. <1856326+iopsthecloud@users.noreply.github.com>
2025-02-10 12:36:38 +0100

    Update delete-posts.js
    fixed modal

--- delete-posts.js
+++ delete-posts.js
@@ -161,7 +161,9 @@ async function init() {
 
 			deleteButton.click();
 
-			const modalAcceptButton = await awaitThisTimed("button.feed-components-shared-decision-modal__confirm-button.artdeco-button.artdeco-button--primary.artdeco-button--2");
+			//const modalAcceptButton = await awaitThisTimed("button.feed-components-shared-decision-modal__confirm-button.artdeco-button.artdeco-button--primary.artdeco-button--2");
+			const modalAcceptButton = await awaitThisTimed(".artdeco-modal .artdeco-button.artdeco-button--primary.artdeco-button--2");
+
 			console.log(modalAcceptButton);
 			if (true !== modalAcceptButton instanceof HTMLElement) {
 				throw "Post deletion modal not found!";
